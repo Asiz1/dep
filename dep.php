27271f7d4a169339e30a96ef7aa2aa59c7d20565
MOSBAHI Abdelaziz <115582191+Asiz1@users.noreply.github.com>
2024-02-02 13:23:50 +0100

    Add files via upload

---
+++ dep.php
@@ -0,0 +1,23 @@
+<?php
+    // Code PHP ici
+    $message = "Bonjour, monde!";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Page Web PHP</title>
+</head>
+<body>
+
+    <h1>Bienvenue sur ma page PHP</h1>
+
+    <?php
+        // Affichage du message PHP dans le code HTML
+        echo "<p>$message</p>";
+    ?>
+
+</body>
+</html>
